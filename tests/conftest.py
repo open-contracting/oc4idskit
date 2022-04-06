c3281ff37bcc32d569d45ddb8e6a396619ae230c
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-04-06 12:20:58 -0400

    test: Use JSON serializer for VCR, instead of ignoring ImportWarning

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,12 @@ logger = logging.getLogger("vcr")
 logger.setLevel(logging.WARNING)
 
 
+@pytest.fixture(scope='module')
+def vcr_config():
+    # Avoid ImportWarning from YAML on PyPy 3.7. https://github.com/yaml/pyyaml/issues/534
+    return {'serializer': 'json'}
+
+
 @pytest.fixture()
 def vcr_cassette_name(request):
     return f"{os.path.splitext(os.path.basename(request.node.fspath))[0]}-{request.node.name}"
