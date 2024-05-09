a3029f796f06c1d325730b794aeef3eab0825e2b
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-05-08 20:19:54 -0400

    test: Add test for warning in combine_project_packages

--- tests/commands/test_combine_project_packages.py
+++ tests/commands/test_combine_project_packages.py
@@ -1,4 +1,8 @@
+import pytest
+
 from oc4idskit.__main__ import main
+from oc4idskit.combine import combine_project_packages
+from oc4idskit.exceptions import MissingProjectsWarning
 from tests import assert_streaming
 
 
@@ -11,3 +15,15 @@ def test_command(capsys, monkeypatch):
         ["project_package_split.json"],
         ["project_package_combined.json"],
     )
+
+
+def test_command_warning(capsys, monkeypatch):
+    with pytest.warns(MissingProjectsWarning) as records:
+        def data():
+            yield {}
+
+        output = combine_project_packages(data())
+
+    assert output == {"uri": "", "publisher": {}, "publishedDate": "", "version": "0.9", "projects": []}
+    assert len(records) == 1
+    assert str(records[0].message) == 'item 0 has no "projects" field (check that it is a project package)'
